f20de135019a83910a9825fdbae94ba4f6948ad0
iykescode <chuksikechukwu15@gmail.com>
2025-05-04 17:56:04 -0700

    commit v4-1 - 05/04/25

--- pages/index.js
+++ pages/index.js
@@ -351,7 +351,7 @@ export default function Home() {
                 <div className="bg-colorDark rounded-t-lg">
                   <BackgroundVideo className="h-[300px] object-cover rounded-t-lg opacity-50" src={PasturePlaceGlobal}/>
                 </div>
-                <div className="p-6 lg:mb-0 md:mb-0 mb-20 h-[192px]">
+                <div className="p-6 lg:mb-0 md:mb-0 mb-16 h-[192px]">
                   <h1 className="text-sm font-black text-primaryColor">WEB APPLICATION</h1>
                   <h1 className="text-xl font-bold">Pasture Place Global</h1>
                   <p className="font-medium">
@@ -628,7 +628,7 @@ export default function Home() {
                 <div className="bg-colorDark rounded-t-lg">
                   <BackgroundVideo className="h-[300px] object-cover rounded-t-lg opacity-50" src={VleetzGiveawayy}/>
                 </div>
-                <div className="p-6 lg:mb-0 md:mb-0 mb-18 h-[192px]">
+                <div className="p-6 lg:mb-0 md:mb-0 mb-16 h-[192px]">
                   <h1 className="text-sm font-black text-primaryColor">FIGMA PROTOTYPE</h1>
                   <h1 className="text-xl font-bold">Vleetz Giveaway V2</h1>
                   <p className="font-medium">
@@ -670,7 +670,7 @@ export default function Home() {
                   <Image src={VleetzGiveaway} alt="Project Image"
                          className="h-[300px] object-cover rounded-t-lg opacity-50"/>
                 </div>
-                <div className="p-6 lg:mb-0 md:mb-0 mb-18 h-[192px]">
+                <div className="p-6 lg:mb-0 md:mb-0 mb-16 h-[192px]">
                   <h1 className="text-sm font-black text-primaryColor">FIGMA PROTOTYPE</h1>
                   <h1 className="text-xl font-bold">Vleetz Giveaway V1</h1>
                   <p className="font-medium">
